895498d505230ebc8f2dbe031365ec4f2588fb98
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2013-08-16 11:54:21 -0700

    use api

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,13 +4,13 @@
 
 angular.module('soulPlayer.controllers', []).
   controller('AlbumListCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('albums/albums.json').success(function(data) {
+    $http.get('http://localhost:8080/albums').success(function(data) {
       $scope.albums = data;
     });
     $scope.orderProp = 'title';
   }]).
   controller('AlbumDetailCtrl', ['$scope', '$routeParams', '$http', 'audioSource', function($scope, $routeParams, $http, audioSource) {
-    $http.get('albums/' + $routeParams.albumId + '.json').success(function(data) {
+    $http.get('http://localhost:8080/albums/:id', {id: "this.id"}).success(function(data) {
       $scope.album = data;
     });
     $scope.play = function(album, song) {
@@ -24,4 +24,9 @@ angular.module('soulPlayer.controllers', []).
     $scope.getSong = function() {
       return audioSource.getSong();
     };
-  }]);
+  }]).
+  controller('AddMusicCtrl', ['$scope', '$routeParams', function($scope, $routeParams] {
+    $http.post('http://localhost:8080/albums', {title: 'title', mp3: 'mp3'}.success(function(){
+      
+    }))
+  });
